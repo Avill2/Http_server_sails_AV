54b0c46aaad3c5032854904103c9f1b518d950c0
Avill2 <andrea.villacis@epn.edu.ec>
2018-06-18 07:45:09 -0500

    actividad de la camara

---
+++ app/src/main/java/com/example/usrdel/http_server_sails_av/ankoActivity.kt
@@ -0,0 +1,16 @@
+package com.example.usrdel.http_server_sails_av
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class ankoActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_anko)
+    }
+
+    fun llamarPos(){
+
+    }
+}
